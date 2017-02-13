a0b3bc76411bb383a22a8c5b32410a24e3d0ee37
Benjamin T. Seaver <BenjaminTSeaver@gmail.com>
2017-02-12 16:43:45 -0800

    Add test case

--- tests/LeapYearGeneratorTest.php
+++ tests/LeapYearGeneratorTest.php
@@ -1,6 +1,6 @@
 <?php
     require_once 'src/LeapYearGenerator.php';
-    
+
     class LeapYearGeneratorTest extends PHPUnit_Framework_TestCase
     {
         function test_isLeapYear_false()
@@ -15,5 +15,19 @@
             // Assert
             $this->assertEquals(false, $result);
         }
+
+        function test_isLeapYear_div4()
+        {
+            // Arrange
+            $test_LeapYearGenerator = new LeapYearGenerator;
+            $input = 2004;
+
+            // Act
+            $result = $test_LeapYearGenerator->isLeapYear($input);
+
+            // Assert
+            $this->assertEquals(true, $result);
+        }
+
     }
 ?>
